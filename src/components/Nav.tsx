a079898b04dcf6a6ac477bf426fcff2b8c703899
github <action@github.com>
2023-12-02 16:19:20 +0000

    Pushing to prod

--- src/components/Nav.tsx
+++
@@ -1,3 +0,0 @@
-export function Nav() {
-    return <nav></nav>;
-}
--- src/types/animeType.ts
+++
@@ -1,6 +0,0 @@
-export type Anime = {
-    id: number;
-    title: string;
-    title_english: string;
-    url_image: string;
-};
